5725f5c3a442a5f66d80d5db8304fc38ce0d0f61
pravesharya <pravesharya925@gmail.com>
2025-01-27 13:47:38 +0530

    renderer Ratio

--- script.js
+++ script.js
@@ -8,13 +8,10 @@ let height = window.innerHeight;
 const canvas = document.querySelector("#canvas");
 const btnM1 = document.querySelector("#m1");
 const btnM2 = document.querySelector("#m2");
-// const btnSI = document.querySelector("#sI");
-// const btnSO = document.querySelector("#sO");
 
-// const modelPath = "./assets/m1.glb";
 const gltfLoader = new GLTFLoader();
 
-let renderer, camera, scene, lightH, lightD, lightA;
+let renderer, camera, scene;
 let model, size, controls;
 
 async function loadModel(modelPath = "./assets/m1.glb") {
@@ -28,45 +25,48 @@ async function loadModel(modelPath = "./assets/m1.glb") {
 
   size = 0.25;
   model.scale.set(size, size, size);
-  model.rotation.set(0, -90, 0);
-  // model.position.set(0, -0.75, 0);
-  // model.castShadow = true;
+  model.rotateY(-Math.PI/2);
   scene.add(model);
 }
 
 async function setupScene() {
+  renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+  renderer.setSize(width, height);
+  renderer.shadowMap.enabled = true;
+  renderer.setPixelRatio(window.devicePixelRatio);
+  
   camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
   camera.position.z = 3;
+  
   scene = new THREE.Scene();
-  renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
-  renderer.shadowMap.enabled = true;
+ 
 
-  lightH = new THREE.HemisphereLight(0xffffff, 1);
+  const lightH = new THREE.HemisphereLight(0xffffff, 10);
   scene.add(lightH);
 
-  // lightD = new THREE.DirectionalLight(0xffffff, 1);
+  // const lightD = new THREE.DirectionalLight(0xffffff, 1);
   // lightD.position.set(0, 1, 0);
   // lightD.target.position.set(0, 0, 0);
   // scene.add(lightD);
 
-  lightA = new THREE.AmbientLight(0x404040);
+  const lightA = new THREE.AmbientLight(0x404040,10);
   scene.add(lightA);
+
+  const floorG = new THREE.PlaneGeometry(3,3);
+  const floorM = new THREE.MeshBasicMaterial({ color: 0xffffff })
+  const floor = new THREE.Mesh(floorG,floorM);
+  scene.add(floor);
+  floorM.side = THREE.DoubleSide;
+  floor.rotateX(-Math.PI/2);
+  floor.position.set(0,-0.29,0);
+  floor.scale.set(0.8,1.2);
 
   controls = new OrbitControls(camera, canvas);
   controls.enableDamping = true;
   controls.dampingFactor = 0.25;
   controls.enableZoom = true;
 
-  // const gltf = await new Promise((resolve) => {
-  //   gltfLoader.load(modelPath, (gltf) => {
-  //     resolve(gltf); // Resolve with entire gltf object, not just scene
-  //   });
-  // });
-  // model = gltf.scene;
-  // console.log(model);
-
   loadModel();
-
   animate();
 }
 setupScene();
@@ -85,21 +85,13 @@ btnM2.addEventListener("click", () => {
   scene.remove(model);
   loadModel("./assets/m2.glb");
 });
-// btnSI.addEventListener("click", () => {
-//   model.scale.x += 0.1;
-//   model.scale.y += 0.1;
-//   model.scale.z += 0.1;
-// });
-// btnSO.addEventListener("click", () => {
-//   model.scale.x -= 0.1;
-//   model.scale.y -= 0.1;
-//   model.scale.z -= 0.1;
-// });
+
 
 window.addEventListener("resize", () => {
   width = window.innerWidth;
   height = window.innerHeight;
   camera.aspect = width / height;
   camera.updateProjectionMatrix();
+  renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(width, height);
 });
